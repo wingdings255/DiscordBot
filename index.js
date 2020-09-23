52eb0b98ebdf4eaaaa6a0fb67dbf856a14c65f2d
wingdings255 <46852882+wingdings255@users.noreply.github.com>
2020-09-23 01:43:21 +0000

    Added base bot files
    Added index.js base bot file
    Added config.json bot config file

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Discord = require('discord.js')
+const config = require("./config.json")
+
+const client = new Discord.Client()
+
+client.once('ready', () => {
+    // Bot ready status log message
+    console.log("[!]: Connected as [ " + client.user.tag + " ]")
+})
+
+client.on('message', message => {
+    //Stops bot from replying to itself
+    if (message.author == client.user) {
+        return
+    }
+
+    // Ping response handler
+    if (message.content === '!ping') {
+        // Sends pong in response to ping message
+        message.channel.send('pong');
+        console.log("[+]: Command [ !ping ] was called by [ " + message.author.tag + "]")
+    }
+});
+
+client.login(config.token)
